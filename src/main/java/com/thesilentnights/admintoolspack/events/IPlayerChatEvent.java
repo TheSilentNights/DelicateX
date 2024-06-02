15442e8a9a4a78a89bcb8709d32e34321353e979
sichen yang <yangsichen080320@outlook.com>
2024-06-02 10:33:56 +0800

    vscode

--- src/main/java/com/thesilentnights/admintoolspack/events/IPlayerChatEvent.java
+++ src/main/java/com/thesilentnights/admintoolspack/events/IPlayerChatEvent.java
@@ -5,9 +5,17 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChannelEvent;
 
+import com.thesilentnights.admintoolspack.AdminToolsPack;
+
+
 public class IPlayerChatEvent implements Listener {
     @EventHandler
     public void PlayerChat(PlayerChannelEvent event){
+        if (AdminToolsPack ) {
+            
+        }
         event.getChannel().replace('&', ChatColor.COLOR_CHAR);
     }
+
+    
 }
