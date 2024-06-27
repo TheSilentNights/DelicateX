2a8c1b8eba51bd7e79783ce37151729e1f62e45c
TheSilentNights <yangsichen080320@outlook.com>
2024-06-27 14:54:07 +0800

    添加了防重复领取

--- src/main/java/com/thesilentnights/delicatex/feature/cdk/CDKModel.java
+++ src/main/java/com/thesilentnights/delicatex/feature/cdk/CDKModel.java
@@ -1,16 +1,20 @@
 package com.thesilentnights.delicatex.feature.cdk;
 
 import com.thesilentnights.delicatex.repo.VaultEssApi;
+import com.thesilentnights.delicatex.utils.messageSender.MessageSender;
+import com.thesilentnights.delicatex.utils.messageSender.messageImp.MessageToSingle;
 import com.thesilentnights.delicatex.utils.task.tick.TickTimer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
+import java.util.Set;
 
 public class CDKModel extends TickTimer {
     private final String key;
     private List<ItemStack> itemStacks;
     private int money;
+    private List<String> gained;
 
     public CDKModel(String key) {
         this.key = key;
@@ -25,12 +29,17 @@ public class CDKModel extends TickTimer {
     }
 
     public void exchange(Player player){
+        if (gained.stream().anyMatch(s -> s.equals(player.getName()))){
+            MessageSender.sendMessage(new MessageToSingle("你已经领取过了哦",player));
+            return;
+        }
         if (!itemStacks.isEmpty()){
             for (ItemStack itemStack : itemStacks) {
               player.getInventory().addItem(itemStack);
             }
         }
         VaultEssApi.essentialsApi.depositPlayer(player,money);
+        gained.add(player.getName());
     }
 
     public void setExpire(Long time){
