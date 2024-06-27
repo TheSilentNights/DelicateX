92d86bd6e7485e4cd4d3646775c978d19bcc5121
TheSilentNights <yangsichen080320@outlook.com>
2024-06-27 14:48:52 +0800

    添加帮助命令

---
+++ src/main/java/com/thesilentnights/delicatex/feature/help/Help.java
@@ -0,0 +1,35 @@
+package com.thesilentnights.delicatex.feature.help;
+
+import com.thesilentnights.delicatex.model.ICommand;
+import com.thesilentnights.delicatex.utils.messageSender.MessageSender;
+import com.thesilentnights.delicatex.utils.messageSender.messageImp.MessageToSingle;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class Help implements ICommand {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        List<String> list = List.of(
+                "/broadcast message",
+                "/cdk create item(需要空时填入none) money(需要空时填入none) cdk兑换码 expire(过期时间/s)",
+                "/cdk remove cdk兑换码",
+                "/distance (两次使用，记录两点坐标)",
+                "/enchant enchantment level(最大255)",
+                "/privateMsg targetPlayer message",
+                "/entityClear x y z (均为范围) type(实体种类，凋落物为item)",
+                "/entityCounter x y z type(可选参数，获取指定种类实体计数)",
+                "/inventoryViewer(简写open) targetPlayer"
+        );
+        list.forEach(string ->MessageSender.sendMessage(new MessageToSingle(string,commandSender)));
+        return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        return List.of();
+    }
+}
