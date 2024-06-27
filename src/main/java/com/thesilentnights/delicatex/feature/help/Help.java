4c52f259ea88d5008a570115530dd1ca8e0dd5f9
TheSilentNights <yangsichen080320@outlook.com>
2024-06-27 14:50:38 +0800

    添加帮助命令

--- src/main/java/com/thesilentnights/delicatex/feature/help/Help.java
+++ src/main/java/com/thesilentnights/delicatex/feature/help/Help.java
@@ -19,10 +19,12 @@ public class Help implements ICommand {
                 "/cdk remove cdk兑换码",
                 "/distance (两次使用，记录两点坐标)",
                 "/enchant enchantment level(最大255)",
-                "/privateMsg targetPlayer message",
                 "/entityClear x y z (均为范围) type(实体种类，凋落物为item)",
                 "/entityCounter x y z type(可选参数，获取指定种类实体计数)",
-                "/inventoryViewer(简写open) targetPlayer"
+                "/inventoryViewer(简写open) targetPlayer",
+                "/ipLocation targetPlayer",
+                "/privateMsg targetPlayer message",
+                "/scheduledReboot time if-broadcast"
         );
         list.forEach(string ->MessageSender.sendMessage(new MessageToSingle(string,commandSender)));
         return true;
