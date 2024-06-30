1db3718726f4c0d0f94e6815d126391de9b0b7e3
TheSilentNights <yangsichen080320@outlook.com>
2024-06-30 20:46:57 +0800

    重制插件命令加载方式

--- src/main/java/com/thesilentnights/delicatex/DelicateX.java
+++ src/main/java/com/thesilentnights/delicatex/DelicateX.java
@@ -5,15 +5,14 @@ import com.thesilentnights.delicatex.feature.cdk.CDK;
 import com.thesilentnights.delicatex.feature.chat.Broadcast;
 import com.thesilentnights.delicatex.feature.chat.PlayerChatListener;
 import com.thesilentnights.delicatex.feature.chat.PrivateMessage;
+import com.thesilentnights.delicatex.feature.chunk.ChunkLoaderCommand;
 import com.thesilentnights.delicatex.feature.distance.Distance;
 import com.thesilentnights.delicatex.feature.enchant.Enchant;
-import com.thesilentnights.delicatex.feature.entity.EntityClear;
-import com.thesilentnights.delicatex.feature.entity.EntityCounter;
-import com.thesilentnights.delicatex.feature.entity.InventoryViewer;
-import com.thesilentnights.delicatex.feature.entity.PlayerJoinListener;
-import com.thesilentnights.delicatex.feature.help.Help;
+import com.thesilentnights.delicatex.feature.entity.*;
+import com.thesilentnights.delicatex.feature.help.Helper;
 import com.thesilentnights.delicatex.feature.ip.IpLocation;
 import com.thesilentnights.delicatex.feature.reboot.ScheduledReboot;
+import com.thesilentnights.delicatex.utils.loader.ModuleLoader;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -24,6 +23,7 @@ public final class DelicateX extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
         //init config
         if (!getDataFolder().exists()) {
             saveDefaultConfig();
@@ -32,30 +32,22 @@ public final class DelicateX extends JavaPlugin {
         instance = this;
 
         //reg command
-        Objects.requireNonNull(getCommand(Distance.COMMAND_NAME)).setExecutor(new Distance());
-        Objects.requireNonNull(getCommand(EntityCounter.COMMAND_NAME)).setExecutor(new EntityCounter());
-        Objects.requireNonNull(getCommand(ScheduledReboot.COMMAND_NAME)).setExecutor(new ScheduledReboot());
-        Objects.requireNonNull(getCommand(EntityClear.COMMAND_NAME)).setExecutor(new EntityClear());
-        Objects.requireNonNull(getCommand(IpLocation.COMMAND_NAME)).setExecutor(new IpLocation());
-        Objects.requireNonNull(getCommand(Broadcast.COMMAND_NAME)).setExecutor(new Broadcast());
-        Objects.requireNonNull(getCommand(Enchant.COMMAND_NAME)).setExecutor(new Enchant());
-        Objects.requireNonNull(getCommand(InventoryViewer.COMMAND_NAME)).setExecutor(new InventoryViewer());
-        Objects.requireNonNull(getCommand(CDK.COMMAND_NAME)).setExecutor(new CDK());
-        Objects.requireNonNull(getCommand(Help.COMMAND_NAME)).setExecutor(new Help());
+        ModuleLoader.registerCommands(this);
 
-        if (Config.getConfig("config").getBoolean("if-enable-private_message")){
-            Objects.requireNonNull(getCommand(PrivateMessage.COMMAND_NAME)).setExecutor(new PrivateMessage());
-        }
         //reg eventsListener
 
         getServer().getPluginManager().registerEvents(new PlayerChatListener(), this);
-        getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerEffectListener(), this);
+
+        //load class
+        com.thesilentnights.delicatex.utils.chunk.ChunkLoader.init();
+
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-
+        com.thesilentnights.delicatex.utils.chunk.ChunkLoader.unloadAllChunks();
         Config.saveConfigs();
     }
 
---
+++ src/main/java/com/thesilentnights/delicatex/utils/loader/ModuleLoader.java
@@ -0,0 +1,51 @@
+package com.thesilentnights.delicatex.utils.loader;
+
+import com.thesilentnights.delicatex.config.Config;
+import com.thesilentnights.delicatex.feature.cdk.CDK;
+import com.thesilentnights.delicatex.feature.chat.Broadcast;
+import com.thesilentnights.delicatex.feature.chat.PrivateMessage;
+import com.thesilentnights.delicatex.feature.chunk.ChunkLoaderCommand;
+import com.thesilentnights.delicatex.feature.distance.Distance;
+import com.thesilentnights.delicatex.feature.enchant.Enchant;
+import com.thesilentnights.delicatex.feature.entity.EntityClear;
+import com.thesilentnights.delicatex.feature.entity.EntityCounter;
+import com.thesilentnights.delicatex.feature.entity.ForceHat;
+import com.thesilentnights.delicatex.feature.entity.InventoryViewer;
+import com.thesilentnights.delicatex.feature.help.Helper;
+import com.thesilentnights.delicatex.feature.ip.IpLocation;
+import com.thesilentnights.delicatex.feature.reboot.ScheduledReboot;
+import com.thesilentnights.delicatex.model.ICommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ModuleLoader {
+    private static final List<ICommand> modules = new ArrayList<>();
+    static { //register module
+        List<ICommand> needRegister = new ArrayList<>(List.of(
+                new CDK(),
+                new Broadcast(),
+                new ChunkLoaderCommand(),
+                new Distance(),
+                new Enchant(),
+                new EntityClear(),
+                new EntityCounter(),
+                new ForceHat(),
+                new InventoryViewer(),
+                new Helper(),
+                new IpLocation(),
+                new ScheduledReboot()
+        ));
+        if (Config.getConfig("config").getBoolean("if-enable-private_message")) {
+            needRegister.add(new PrivateMessage());
+        }
+        modules.addAll(needRegister);
+    }
+    public static void registerCommands(JavaPlugin plugin){
+        modules.forEach(command -> {
+            Objects.requireNonNull(plugin.getCommand(command.getClass().getSimpleName())).setExecutor(command);
+        });
+    }
+}
