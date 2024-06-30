83d9a3733f67a419d0b7f78e4bd6f42b8c7a3d2b
TheSilentNights <yangsichen080320@outlook.com>
2024-06-30 20:45:20 +0800

    添加区块加载/卸载方法

---
+++ src/main/java/com/thesilentnights/delicatex/feature/chunk/ChunkLoaderCommand.java
@@ -0,0 +1,83 @@
+package com.thesilentnights.delicatex.feature.chunk;
+
+import com.thesilentnights.delicatex.DelicateX;
+import com.thesilentnights.delicatex.model.ICommand;
+import com.thesilentnights.delicatex.utils.chunk.ChunkLoader;
+import com.thesilentnights.delicatex.utils.messageSender.MessageSender;
+import com.thesilentnights.delicatex.utils.messageSender.messageImp.MessageToSingle;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChunkLoaderCommand implements ICommand {
+    public static final String COMMAND_NAME = "chunkLoader";
+    //param: load world x z ifGenerate
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        if (strings.length != 5) {
+            return false;
+        }
+
+        int x, z;
+        boolean ifGenerate;
+        try {
+            x = Integer.parseInt(strings[2]);
+            z = Integer.parseInt(strings[3]);
+            ifGenerate = Boolean.parseBoolean(strings[4]);
+        } catch (NumberFormatException e) {
+            MessageSender.sendMessage(new MessageToSingle("坐标错误", commandSender));
+            return true;
+        }
+        World world = DelicateX.getInstance().getServer().getWorld(strings[1]);
+        if (world == null){
+            MessageSender.sendMessage(new MessageToSingle("世界不存在",commandSender));
+            return true;
+        }
+        switch (strings[0]) {
+            case "load" -> {
+                ChunkLoader.loadChunk(world, x, z, ifGenerate);
+            }
+            case "unload" -> {
+                ChunkLoader.unloadChunk(world, x, z);
+            }
+            default -> {
+                MessageSender.sendMessage(new MessageToSingle("该操作不存在",commandSender));
+            }
+        }
+        return true;
+
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        switch (strings.length){
+            case 1->{
+                return List.of("load","unload");
+            }
+            case 2->{
+                List<String> result = new ArrayList<>();
+                DelicateX.getInstance().getServer().getWorlds().forEach(world -> {
+                    result.add(world.getName());
+                });
+                return result;
+            }
+            case 3->{
+                return List.of("[x]");
+            }
+            case 4->{
+                return List.of("[z]");
+            }
+            case 5->{
+                return List.of("[ifGenerate]");
+            }
+            default->{
+                return List.of("");
+            }
+        }
+    }
+}
---
+++ src/main/java/com/thesilentnights/delicatex/utils/chunk/ChunkLoader.java
@@ -0,0 +1,34 @@
+package com.thesilentnights.delicatex.utils.chunk;
+
+import org.bukkit.Chunk;
+import org.bukkit.World;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChunkLoader {
+    static List<Chunk> chunks;
+
+    public static void init(){
+        chunks = new ArrayList<>();
+    }
+    public static void loadChunk(World world, int x, int z, boolean generate/*不存在时是否生成*/) {
+        world.loadChunk(x, z, generate);
+        chunks.add(world.getChunkAt(x, z, generate));
+    }
+
+    public static void unloadChunk(World world,int x,int z) {
+        if (world.getChunkAt(x,z).isLoaded()){
+            world.getChunkAt(x,z).unload(true);
+        }
+    }
+
+    //卸载所有手动加载的区块
+    public static void unloadAllChunks() {
+        chunks.forEach(chunk -> {
+            if (chunk.isLoaded()){
+                chunk.unload(true);
+            }
+        });
+    }
+}
